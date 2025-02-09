d79bbf60cefd778a339934ae92b4ce30d59ef7c4
Benjamin Dos Santos <hello@benjamindossantos.be>
2025-02-09 16:02:44 +0000

    chore: rename project in devcontainer and compose

--- strapi/config/database.ts
+++ strapi/config/database.ts
@@ -2,7 +2,7 @@ export default ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', 'database'),
+      host: env('DATABASE_HOST', 'strapi-db'),
       port: env.int('DATABASE_PORT', 5432),
       database: env('DATABASE_NAME', 'postgres'),
       user: env('DATABASE_USERNAME', 'postgres'),
